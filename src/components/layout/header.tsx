774428912d2b787fdd43444d00e9062d1fa4eba5
Giovanni Leo <giovannileo100@gmail.com>
2026-06-01 12:43:28 +0800

    feat: added the company logo on the header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,7 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/src/lib/utils";
 import { Button, Flex } from "../ui";
 import { useState } from "react";
+import Image from "next/image";
 
 type Item = {
   name: string;
@@ -38,10 +39,11 @@ const Header = () => {
     >
       <Flex className="w-full items-center justify-between gap-4">
         <Link
-          className="font-bold text-brand-primary tracking-[-0.02em] cursor-pointer hover:opacity-70 transition-all duration-200 ease-linear"
+          className="flex items-center gap-2 font-bold text-brand-primary tracking-[-0.02em] cursor-pointer hover:opacity-70 transition-all duration-200 ease-linear"
           href="/"
         >
-          Upsert Solution
+          <Image src="/favicon.ico" width={24} height={24} alt="Logo" />
+          <p>Upsert Solution</p>
         </Link>
         <Flex className="mx-auto hidden md:flex items-center" gap="sm">
           {navItems.map((item) => (
@@ -104,9 +106,11 @@ const Header = () => {
               {item.name}
             </Link>
           ))}
-          <Button size="sm" className="w-full" onClick={() => setOpenForPath(null)}>
-            Get Started
-          </Button>
+          <Link href="/contact">
+            <Button size="sm" className="w-full" onClick={() => setOpenForPath(null)}>
+              Get Started
+            </Button>
+          </Link>
         </Flex>
       </div>
     </Flex>
